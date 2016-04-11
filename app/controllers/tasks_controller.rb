78c0db0b73ed140dfa40657e5bff611bd23a5c0a
Vitamin1899 <vitalimarinich@gmail.com>
2016-04-11 19:06:32 +0300

    implemented editing tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,11 +2,25 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
+    render :show_form
   end
 
   def create
     @task = Task.create(task_params)
     @tasks = Task.all
+    render :hide_form
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    render :show_form
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes(task_params)
+    @tasks = Task.all
+    render :hide_form
   end
 
   def destroy
