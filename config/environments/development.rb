e8f915650f3a464e30483c70d57e2fb12b7192a2
Vitamin1899 <vitalimarinich@gmail.com>
2016-04-28 00:25:27 +0300

    omniauth&devise: configured initializers, environments, routes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,9 +16,26 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # General Settings
+  config.app_domain = 'somedomain.com'
+
+  # Email
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: config.app_domain }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    enable_starttls_auto: true,
+    user_name: 'someuser',
+    password: 'somepass',
+    authentication: :plain,
+    domain: 'somedomain.com'
+  }
+
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
