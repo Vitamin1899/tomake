2917da921e09c7ab29ff6161891a9a3f8a463107
Vitamin1899 <vitalimarinich@gmail.com>
2016-05-04 00:08:05 +0300

    omniauth&devise: finish signup

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
+  get '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
